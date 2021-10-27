cb2d8467003d111994a184e8d8b675162679b59b
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2021-10-27 20:59:48 +0700

    Add clients page (WIP)

---
+++ src/components/client-box/index.tsx
@@ -0,0 +1,278 @@
+import React, { FC, ReactNode } from "react";
+
+import * as styles from "./index.module.scss";
+
+export interface ClientBoxProps {
+  size?: 1 | 2 | 3;
+  title: string;
+  logo: ReactNode;
+  industry: string;
+  services: string[];
+  description: string;
+  testimonialName?: string;
+  testimonialJobtitle?: string;
+  backgroundColor?: string;
+  backgroundImage?: ReactNode;
+  backgroundImageMobile?: ReactNode;
+  backgroundImagePosition?: "top" | "bottom";
+  arrow: "dark" | "light";
+  pageUrl?: string;
+}
+
+export const ClientBox: FC<ClientBoxProps> = (props) => {
+  const { size } = props;
+
+  switch (size) {
+    case 1:
+      return <ClientBoxSm {...props} />;
+    case 2:
+      return <ClientBoxMd {...props} />;
+    case 3:
+      return <ClientBoxLg {...props} />;
+  }
+};
+
+const ClientBoxSm: FC<ClientBoxProps> = (props) => {
+  const {
+    backgroundColor,
+    pageUrl,
+    logo,
+    title,
+    industry,
+    description,
+    services,
+  } = props;
+  return (
+    <div className="client-box client-box__sm">
+      <div className="client-box__logo" style={{ backgroundColor }}>
+        {pageUrl && (
+          <a
+            href="{{client.page_url}}"
+            className="client-box__logo__arrow d-lg-flex d-none"
+          >
+            <img
+              className="client-box__logo__arrow--non_hover"
+              src="/assets/images/clients/link_arrow-{{client.arrow}}.svg"
+            />
+            <div className="client-box__logo__arrow--hover">
+              <img src="/assets/images/clients/link_arrow-light.svg" />
+              <span>See Case Study</span>
+            </div>
+          </a>
+        )}
+        {logo}
+        <div className="client-box__content">
+          <h3 className="client-box__content__title">{title}</h3>
+          <p className="client-box__content__industry">{industry}</p>
+          <p className="client-box__content__description">{description}</p>
+          <div className="d-block d-lg-none">
+            {pageUrl && (
+              <a
+                className="client-box__link_mobile btn btn-primary d-flex justify-content-between"
+                href="{{client.page_url}}"
+              >
+                See Case Study
+                <img
+                  className="client-box__link_mobile__arrow"
+                  src="/assets/images/clients/link_arrow-light.svg"
+                />
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="client-box__services d-lg-flex d-none">
+          <div className="client-box__services__titlebox">
+            <p>We worked on:</p>
+          </div>
+          <div className="client-box__services__valuebox">
+            <p>{services.map((s) => `#${s}`).join(" ")}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ClientBoxMd: FC<ClientBoxProps> = (props) => {
+  const {
+    backgroundColor,
+    backgroundImage,
+    pageUrl,
+    logo,
+    title,
+    industry,
+    description,
+    services,
+    testimonialName,
+    testimonialJobtitle,
+  } = props;
+
+  return (
+    <div className="client-box client-box__md">
+      <div className="client-box__md__bg">{backgroundImage}</div>
+      <div className="client-box__logo" style={{ backgroundColor }}>
+        {pageUrl && (
+          <a
+            href="{{client.page_url}}"
+            className="client-box__logo__arrow d-lg-flex d-none"
+          >
+            <img
+              className="client-box__logo__arrow--non_hover"
+              src="/assets/images/clients/link_arrow-{{client.arrow}}.svg"
+            />
+            <div className="client-box__logo__arrow--hover">
+              <img src="/assets/images/clients/link_arrow-light.svg" />
+              <span>See Case Study</span>
+            </div>
+          </a>
+        )}
+
+        {logo}
+      </div>
+      <div className="client-box__content">
+        <h3 className="client-box__content__title">{title}</h3>
+        <p className="client-box__content__industry">{industry}</p>
+        <p className="client-box__content__description">{description}</p>
+        {testimonialName && (
+          <div className="client-box__testimonial">
+            <img
+              className="client-box__testimonial__line"
+              src="/assets/images/clients/testimonial_line.svg"
+            />
+            <p className="client-box__testimonial__name">{testimonialName}</p>
+            <p className="client-box__testimonial__jobtitle">
+              {testimonialJobtitle}
+            </p>
+          </div>
+        )}
+
+        <div className="d-block d-lg-none">
+          {pageUrl && (
+            <a
+              className="client-box__link_mobile btn btn-primary d-flex justify-content-between"
+              href="{{client.page_url}}"
+            >
+              See Case Study
+              <img
+                className="client-box__link_mobile__arrow"
+                src="/assets/images/clients/link_arrow-light.svg"
+              />
+            </a>
+          )}
+        </div>
+      </div>
+      <div className="client-box__services d-lg-flex d-none">
+        <div className="client-box__services__titlebox">
+          <p>We worked on:</p>
+        </div>
+        <div className="client-box__services__valuebox">
+          <p>{services.map((s) => `#${s}`).join(" ")}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ClientBoxLg: FC<ClientBoxProps> = (props) => {
+  const {
+    backgroundColor,
+    backgroundImage,
+    backgroundImageMobile,
+    pageUrl,
+    logo,
+    title,
+    industry,
+    description,
+    services,
+    testimonialName,
+    testimonialJobtitle,
+  } = props;
+
+  return (
+    <div className="client-box__lg">
+      <div
+        className="mobile-bg d-block d-lg-none"
+        style={{ backgroundImage: `url('${backgroundImageMobile}')` }}
+      ></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 px-0">
+            <div className="client-box__logo" style={{ backgroundColor }}>
+              {logo}
+            </div>
+          </div>
+          <div className="col-lg-8 pl-4 pr-0">
+            {pageUrl && (
+              <a
+                href="{{client.page_url}}"
+                className="client-box__logo__arrow d-none d-lg-block d-lg-flex d-none"
+              >
+                <img
+                  className="client-box__logo__arrow--non_hover"
+                  src="/assets/images/clients/link_arrow-{{client.arrow}}.svg"
+                />
+                <div className="client-box__logo__arrow--hover">
+                  <img src="/assets/images/clients/link_arrow-light.svg" />
+                  <span>See Case Study</span>
+                </div>
+              </a>
+            )}
+
+            <div className="client-box__content">
+              <div className="client-box__content__inner">
+                <h3 className="client-box__content__title">{title}</h3>
+                <p className="client-box__content__industry">{industry}</p>
+                <p className="client-box__content__description">
+                  {description}
+                </p>
+              </div>
+              {testimonialName && (
+                <div className="client-box__testimonial">
+                  <img
+                    className="client-box__testimonial__line"
+                    src="/assets/images/clients/testimonial_line.svg"
+                  />
+                  <p className="client-box__testimonial__name">
+                    {testimonialName}
+                  </p>
+                  <p className="client-box__testimonial__jobtitle">
+                    {testimonialJobtitle}
+                  </p>
+                </div>
+              )}
+
+              <div className="d-block d-lg-none">
+                {pageUrl && (
+                  <a
+                    className="client-box__link_mobile btn btn-primary d-flex justify-content-between"
+                    href="{{client.page_url}}"
+                  >
+                    See Case Study
+                    <img
+                      className="client-box__link_mobile__arrow"
+                      src="/assets/images/clients/link_arrow-light.svg"
+                    />
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="client-box__services row d-none d-lg-flex">
+          <div className="col-lg-4 px-0 client-box__services__titlebox">
+            <p>We worked on:</p>
+          </div>
+          <div className="col-lg-8 px-0 client-box__services__valuebox">
+            <p>
+              {services.map((s, i) => (
+                <span key={s + i} className="mr-4">
+                  #{s}
+                </span>
+              ))}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,7 +1,347 @@
+import { ClientBox, ClientBoxProps } from "@app/components/client-box";
 import React from "react";
 
+import "./index.scss";
+
+const services = [
+  "All",
+  "Webdesign",
+  "Paid ads",
+  "Onboarding",
+  "Content marketing",
+  "Training",
+  "Activation",
+  "Copywriting",
+  "Analytics",
+  "Photo/Video Production",
+  "B2B Lead Generation",
+  "Strategy",
+];
+const industries = ["All", "SaaS", "Mobile", "FinTech", "E-commerce", "B2B"];
+
+const clients: ClientBoxProps[] = [
+  {
+    title: "TikTok",
+    logo: "/assets/images/clients/tiktok-logo.jpg",
+    industry: "Mobile",
+    services: ["Webdesign", "Copywriting", "Onboarding"],
+    description:
+      "Something wasn't adding up. Based on TikTok's unreal success, businesses should have been flooding in to partner up as advertisers...<br> <br>Now things are starting to pick up, and TikTok's success is becoming whole again.",
+    testimonialName: null,
+    testimonialJobtitle: null,
+    backgroundColor: "#000",
+    backgroundImage: "/assets/images/clients/tiktok_background.jpg",
+    backgroundImageMobile:
+      "/assets/images/clients/tiktok_background-mobile.jpg",
+    arrow: "light",
+    size: 3,
+  },
+  {
+    title: "Sendcloud",
+    logo: "/assets/images/clients/sendcloud-logo.png",
+    industry: "SaaS",
+    services: ["Copywriting", "Training", "Content marketing"],
+    description:
+      "Read the full story to see how we've led Sendcloud on a copywriting journey that’s impacted their messaging in more than six languages...",
+    backgroundColor: "#EEF6FF",
+    arrow: "dark",
+    size: 1,
+  },
+  {
+    title: "Peg",
+    logo: "/assets/images/clients/peg-logo.svg",
+    industry: "SaaS",
+    services: ["B2B Lead Generation", "Copywriting", "Activation"],
+    description:
+      "Over in London, we worked with Peg to build a stable sales infrastructure and an outbound lead generation engine. They went on to raise over $1,000,000 in their series A.",
+    backgroundColor: "#E94058",
+    arrow: "light",
+    size: 1,
+  },
+  {
+    title: "InsectoCycle",
+    logo: "/assets/images/clients/insecto_cycle-logo.svg",
+    industry: "B2B",
+    services: [
+      "Webdesign",
+      "Onboarding",
+      "Copywriting",
+      "Photo/Video Production",
+      "Analytics",
+      "Strategy",
+    ],
+    description:
+      "We’ve helped InsectoCycle create their business case, brand identity and website. A full-service job!",
+    backgroundColor: "#E4F0EC",
+    arrow: "dark",
+    size: 1,
+  },
+  {
+    title: "Alpian",
+    logo: null,
+    industry: "FinTech",
+    services: [
+      "Copywriting",
+      "Content marketing",
+      "Paid Ads",
+      "Onboarding",
+      "Analytics",
+      "Activation",
+      "Strategy",
+    ],
+    description:
+      "Alpian aims to change banking and investing in the most competitive market in the world... Switzerland. We are building explosive momentum to gain a piece of that market.",
+    testimonialName: null,
+    testimonialJobtitle: null,
+    backgroundImage: "/assets/images/clients/Alpian_background.svg",
+    arrow: "light",
+    size: 2,
+  },
+  {
+    title: "Switch.CM",
+    logo: "/assets/images/clients/switchcm-logo.svg",
+    industry: "SaaS",
+    services: ["B2B Lead Generation", "Onboarding", "Copywriting", "Strategy"],
+    description:
+      "We built Switch an automated customer acquisition engine. The result? A triple-digit number of leads within three months and Switch being acquired in eight!",
+    backgroundColor: "#7197BB",
+    arrow: "dark",
+    size: 1,
+  },
+  {
+    title: "Klooker",
+    logo: "/assets/images/clients/klooker-logo.svg",
+    industry: "E-commerce",
+    services: [
+      "Webdesign",
+      "Paid ads",
+      "Activation",
+      "Photo/Video Production",
+      "Analytics",
+      "Onboarding",
+      "Strategy",
+    ],
+    description:
+      "“Double’s team is extremely forthcoming with ideas to engage our customers further.“",
+    testimonialName: "Dennis Kamst",
+    testimonialJobtitle: "Founder & CEO at klooker",
+    backgroundColor: "#725D87",
+    backgroundImage: "/assets/images/clients/klooker_background.jpg",
+    backgroundImageMobile:
+      "/assets/images/clients/klooker_background-mobile.jpg",
+    arrow: "light",
+    size: 3,
+  },
+  {
+    title: "BABB",
+    logo: "/assets/images/clients/babb-logo.svg",
+    industry: "FinTech",
+    services: ["Strategy", "Copywriting", "Paid Ads"],
+    description:
+      "Pieter gave BABB a hand in the early days to establish their messaging and market positioning. Their initial coin offering (ICO) raised $25m ;)",
+    backgroundColor: "#56204B",
+    arrow: "dark",
+    size: 1,
+  },
+  {
+    title: "Elugie",
+    logo: "/assets/images/clients/elugie-logo.svg",
+    industry: "B2B",
+    services: [
+      "Strategy",
+      "Copywriting",
+      "Activation",
+      "Onboarding",
+      "B2B Lead Generation",
+    ],
+    description:
+      "Elugie are set on selling 100,000 of their hydrogen power plants by 2030. Read the case study to see exactly how they got on the path to selling at scale!",
+    backgroundImage: "/assets/images/clients/elugie_background.svg",
+    backgroundImagePosition: "bottom",
+    arrow: "light",
+    size: 2,
+  },
+  {
+    title: "Core Life Analytics",
+    logo: "/assets/images/clients/core_life_analytics-logo.svg",
+    industry: "SaaS",
+    services: [
+      "Webdesign",
+      "Analytics",
+      "Copywriting",
+      "Photo/Video Production",
+      "Onboarding",
+    ],
+    description:
+      "Core Life Analytics dreamed of helping scientists around the world. So we built them sales funnels and a video to get them on the way. Check out the full case!",
+    backgroundColor: "#751162",
+    arrow: "light",
+    size: 1,
+  },
+  {
+    title: "Diesel",
+    logo: "/assets/images/clients/diesel-logo.svg",
+    industry: "E-commerce",
+    services: ["Strategy", "Copywriting", "Analytics"],
+    description:
+      "Arthur, our conversion optimisation geek, helped Diesel explode their online revenue in Australia by 44% in three months.",
+    backgroundColor: "#000000",
+    arrow: "light",
+    size: 1,
+  },
+  {
+    title: "Sigrow",
+    logo: "/assets/images/clients/sigrow-logo.svg",
+    industry: "B2B",
+    services: [
+      "Webdesign",
+      "Copywriting",
+      "Paid Ads",
+      "Activation",
+      "Analytics",
+    ],
+    description:
+      "We helped Sigrow from p/m-fit onwards, to scale up their brand and bring in a steady and predictable flow of leads for their business.",
+    backgroundColor: "#FFFFFF",
+    arrow: "light",
+    size: 1,
+  },
+];
+
 export const ClientsPage = () => {
-  return <h1>Clients</h1>;
+  const rows = clients.reduce(
+    (prev, curr) => {
+      const lastIndex = prev.length - 1;
+      const lastRow = prev[lastIndex];
+      const sum = lastRow.reduce((p, c) => p + c.size, 0);
+      if (sum >= 3) {
+        prev[lastIndex + 1] = [curr];
+      } else {
+        prev[lastIndex].push(curr);
+      }
+      return prev;
+    },
+    [[]] as Array<ClientBoxProps[]>
+  );
+
+  console.log({ rows });
+  return (
+    <div id="clients-page">
+      {/* Hero */}
+      <section id="hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 d-flex justify-content-center justify-content-lg-between align-items-center">
+              {/* Title */}
+              <h1 className="mb-4 mb-lg-0">Clients</h1>
+
+              {/* Service filters */}
+              <div className="service_filters dropdown d-none d-lg-block">
+                <button
+                  className="service_filters__btn btn btn-light dropdown-toggle"
+                  type="button"
+                  id="service_filters"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <img
+                    className="mr-2"
+                    src="/assets/images/clients/chevron-down.svg"
+                  />
+                  #<span v-html="selectedServiceLabel()"></span>
+                </button>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="service_filters"
+                >
+                  {services.map((s, i) => (
+                    <button key={s + i} className="dropdown-item">
+                      {s}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Industry filters */}
+          <div className="row">
+            <div className="col-lg-12">
+              <div
+                className="industry_filters btn-group d-none d-lg-flex"
+                role="group"
+              >
+                {industries.map((industry, i) => (
+                  <button
+                    key={industry}
+                    type="button"
+                    className="industry_filters__item"
+                  >
+                    {industry}
+                  </button>
+                ))}
+              </div>
+
+              {/* Industry filters mobile */}
+              <div
+                className="accordion industry_filters-mobile d-block d-lg-none"
+                id="industry_filters"
+              >
+                <button
+                  className="industry_filters-mobile__mainbtn collapsed"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseOne"
+                  aria-expanded="false"
+                  aria-controls="collapseOne"
+                >
+                  Show <span v-html="selectedIndustry"></span>
+                  <img
+                    className="mr-2 industry_filters-mobile__mainbtn__arrow"
+                    src="/assets/images/clients/chevron-down-light.svg"
+                  />
+                </button>
+                <div
+                  className="collapse"
+                  id="collapseOne"
+                  aria-labelledby="headingOne"
+                  data-parent="#industry_filters"
+                >
+                  {industries.map((industry, i) => (
+                    <button
+                      type="button"
+                      className="industry_filters-mobile__item"
+                    >
+                      {industry}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Clients */}
+      <section className="client-boxes pt-0">
+        {rows.map((row, i) => {
+          if (row.length === 1) {
+            return <ClientBox {...row[0]} />;
+          }
+
+          return (
+            <div className="container">
+              <div className="row">
+                {row.map((client, ii) => (
+                  <ClientBox {...client} />
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default ClientsPage;
