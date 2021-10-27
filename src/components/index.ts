472e63953f28770f1f50569e5952355f59ff24ce
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2021-10-27 08:51:38 +0700

    add arrowbutton (WIP)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./header";
 export * from "./footer";
 export * from "./seo";
 export * from "./animated-line";
+export * from "./link";
---
+++ src/components/link/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { GatsbyLinkProps, Link as GatsbyLink } from "gatsby";
+
+export interface LinkProps extends GatsbyLinkProps<any> {}
+
+export const Link: FC<LinkProps> = ({ to, children, ref, ...rest }) => {
+  // External link
+  if (to.startsWith("http")) {
+    return (
+      <a href={to} target="_blank" rel="noopener" {...rest}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <GatsbyLink to={to} {...rest}>
+      {children}
+    </GatsbyLink>
+  );
+};
