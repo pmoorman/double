2313954496bd0653a40798a200ec2409f28f6ee2
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2021-12-10 16:11:15 +0700

    disable pricing section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const IndexPage = () => {
       <AcademySection />
       <AboutSection />
       <ProcessSection />
-      <PricingSection />
+      {/* <PricingSection /> */}
       <BottomSection />
     </>
   );
