ae11613a5343b0dd7ca4cb0f13d7d569fca8e1db
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2022-07-18 14:06:39 +0700

    Add Reddit pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-reddit-pixel`,
+      options: {
+        pixelId: "t2_odexgo3r",
+      },
+    },
+    {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-M44383W",
